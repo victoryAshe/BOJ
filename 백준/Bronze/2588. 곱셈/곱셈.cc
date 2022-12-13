5890de1785cb86ba64d98e363045bc0d4273d6a6
Yeon <includeashe@gmail.com>
2022-12-13 17:43:10 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    
+    cout << a * (b % 10) << "\n"
+        << a * (b / 10 % 10) << "\n"
+        << a * (b / 100) << "\n"
+        << a * (b % 10) + a * (b / 10 % 10) *10 + a * (b / 100) * 100;
+
+    
+    return 0;
+}
