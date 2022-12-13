9b219b14707ec60a97281ef608eafe5aeda368e4
Yeon <includeashe@gmail.com>
2022-12-13 16:54:01 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name;
+    cin >> name;
+    
+    string result = name + "??!";
+    cout << result;
+    
+    return 0;
+}
