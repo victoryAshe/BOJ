388c6ff40fe1522d1e7a373292ff7f24ef4e2dd6
Yeon <includeashe@gmail.com>
2022-12-13 16:52:06 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int a; int b;
+    cin >> a >> b;
+    
+    cout << a + b << "\n" << a - b << "\n"
+        << a * b << "\n" << a / b << "\n" << a % b;
+    
+    return 0;
+}
