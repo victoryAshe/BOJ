b09f83e85aae7f2dbfb1ca36b9555c969d6bceaf
Yeon <includeashe@gmail.com>
2022-12-13 15:17:14 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    string star = "";
+    for(int i = 0; i<n; i++)
+    {
+        star+='*';
+        cout << star << endl;
+    }
+    
+    return 0;
+}
