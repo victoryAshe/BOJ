79e04100fbb835e9db0844ee9a0aef3d696e4056
Yeon <includeashe@gmail.com>
2024-08-02 23:54:45 +0900

    [Silver V] Title: 불사조, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/31780. 불사조/불사조.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int X , M;
+    cin >> X >> M;
+    cout << X*(M+1)<<"\n";
+    
+    return 0;
+}
