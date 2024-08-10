af04528bdfe725f4e3f91f4106d0e6c4bfe2ffc8
Yeon <includeashe@gmail.com>
2024-08-10 23:57:31 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int N; cin >> N;
+    cout << N-1946 << "\n";
+    
+    return 0;
+}
