40632a0fcae1584b3e8bb682b781ca289551a625
Yeon <includeashe@gmail.com>
2024-08-18 06:40:07 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 44 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Point
+{
+public:
+    int x, y;
+};
+
+bool compare(const Point& a, const Point& b)
+{
+    if (a.y == b.y) return a.x < b.x;
+    return a.y < b.y;
+}
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N; cin >> N;
+    int x, y; vector<Point> points;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> x >> y;
+        Point point;
+        point.x = x; point.y = y;
+        points.push_back(point);
+    }
+    sort(points.begin(), points.end(), compare);
+    for (Point p : points)
+    {
+        cout << p.x << " " << p.y << "\n";
+    }
+
+    return 0;
+}
