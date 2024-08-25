6670a4e077623eb28fea9de13ca42f9fa9bce457
Yeon <includeashe@gmail.com>
2024-08-26 00:04:25 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{   
+    int N; cin >> N;
+    string s = "";
+    cin >> s;
+    int result = 0;
+    for( int i = 0; i<N; i++)
+    {
+        result+= s[i]-48;
+    }
+    cout << result;
+    
+    return 0;
+}
