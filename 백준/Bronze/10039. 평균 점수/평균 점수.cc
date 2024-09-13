04e4724d3af8369564771e86d583679737daea1e
Yeon <includeashe@gmail.com>
2024-09-13 22:36:35 +0900

    [Bronze IV] Title: 평균 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, sum = 0;
+    for(int i = 0; i< 5; i++)
+    {
+        cin >> num;
+        if(num<40) num = 40;
+        sum+=num;
+    }
+    cout << sum/5;
+    
+    return 0;
+}
