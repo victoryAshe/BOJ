93e6dfd0b4e66f6fdd5ab29e2dce4ecb939d22bb
Yeon <includeashe@gmail.com>
2024-10-04 20:57:59 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+const int MAX = 9;
+bool visit[MAX];
+vector<int> adj[MAX];
+queue<int> Q;
+
+void BFS(int start)
+{
+    memset(visit, false, sizeof(visit));
+    visit[start] = true;
+    Q.push(start);
+
+    while (!Q.empty())
+    {
+        int x = Q.front();
+        Q.pop();
+
+        int size = adj[x].size();
+        for (int i = 0; i < size; i++)
+        {
+            int next = adj[x][i];
+            if (!visit[next])
+            {
+                visit[next] = true;
+                Q.push(next);
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N; cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            int num; cin >> num;
+            // 오른쪽 이동
+            if (j + num < N) adj[i * N + j].push_back(i * N + j + num);
+            // 아래쪽 이동
+            if (i * N + j + num * N <= N*N-N+j)  adj[i * N + j].push_back(i * N + j + num * N);
+        }
+    }
+
+    BFS(0);
+
+    if (visit[N * N - 1] == true) cout << "HaruHaru";
+    else cout << "Hing";
+
+    return  0;
+}
