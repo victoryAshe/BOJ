1028d13167f5c772eed3902bd404be14cc7a297d
Yeon <includeashe@gmail.com>
2024-10-23 00:58:19 +0900

    [Gold V] Title: 선 긋기, Time: 368 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.cc
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -1,66 +1,60 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 using namespace std;
 
-bool visit[1000001] = { false, };
+pair<int, int> lines[1000000];
 
 bool comp(pair<int, int>& a, pair<int, int>& b)
 {
-	return a.first != b.first ? a.first < b.first : a.second < b.second;
+	if (a.first != b.first) return a.first < b.first;
+	return a.second < b.second;
 }
 
-int isOverlapped(pair<int, int>& a, pair<int, int>& b)
-{
-	if (b.first <= a.second && b.second <= a.second) return -1; // a에 b가 포함됨
-	if (b.first <= a.second) return 1;// b가 a와 일부 겹침
-	return 0; // 겹치지 않음
-}
+enum class OverlapState {
+	겹치지않음, 
+	일부겹침, 
+	포함됨
+};
 
-int len(int a, int b)
+OverlapState overlapResult(pair<int,int>& a, pair<int,int>& b)
 {
-	return b - a > 0 ? b - a : -(b - a);
+	if (b.first <= a.second)
+	{
+		if (a.second <= b.second) return OverlapState::일부겹침;
+		return OverlapState::포함됨;
+	}
+	else return OverlapState::겹치지않음;
 }
 
 int main()
 {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
-
-	vector<pair<int, int>> lines;
+	int answer = 0;
 	int N; cin >> N;
 	for (int i = 0; i < N; i++)
 	{
 		int x, y;
 		cin >> x >> y;
-		lines.push_back(make_pair(x, y));
+		lines[i] = make_pair(x, y);
 	}
-
-	sort(lines.begin(), lines.end(), comp);
-
-	int result = 0;
-	for (int i = 0; i < N; i++)
+	sort(lines, lines + N, comp);
+    
+	pair<int, int> prevLine = lines[0];
+	for (int i = 1; i < N; i++)
 	{
-		if (visit[i]) continue;
-		pair<int, int> overlapTarget = lines[i];
-		visit[i] = true;
-		for (int j = i + 1; j < N; j++)
+		OverlapState state = overlapResult(prevLine, lines[i]);
+		if (state == OverlapState::일부겹침)
 		{
-			int overlapResult = isOverlapped(overlapTarget, lines[j]);
-			if (overlapResult == 0) break;
-			else if (overlapResult == 1)
-			{
-				if (overlapTarget.second < lines[j].second)
-				{
-					overlapTarget.second = lines[j].second;
-				}
-			}
-
-			visit[j] = true;
+			prevLine.second = lines[i].second;
 		}
-		result += len(overlapTarget.first, overlapTarget.second);
+		else if (state == OverlapState::겹치지않음)
+		{
+			answer += prevLine.second - prevLine.first;
+			prevLine = lines[i];
+		}
 	}
-	cout << result;
-
+	answer += prevLine.second - prevLine.first;
+	cout << answer;
 	return 0;
 }
