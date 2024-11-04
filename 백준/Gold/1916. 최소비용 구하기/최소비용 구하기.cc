4e7af070e6ac428d0de3060e6b65c12069093029
Yeon <includeashe@gmail.com>
2024-11-04 21:36:08 +0900

    [Gold V] Title: 최소비용 구하기, Time: 32 ms, Memory: 6288 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+bool visit[1001];
+long long dist[1001];
+vector<pair<int,int>> adj[1001]; // [start]: cost, end
+
+void BFS(int start)
+{
+	priority_queue<pair<long long, int>> pq;
+	pq.push({0, start});
+	visit[start] = true;
+	while (!pq.empty())
+	{
+		long long curDist = -pq.top().first;
+		int cur = pq.top().second;
+		pq.pop();
+		if (curDist > dist[cur]) continue;
+		int size = adj[cur].size();
+		for (int i = 0; i < size; i++)
+		{
+			pair<int, int> next = adj[cur][i];
+			int nextDist = curDist + next.first;
+			if (!visit[next.second] || dist[next.second] >nextDist)
+			{
+				dist[next.second] = nextDist;
+				visit[next.second] = true;
+				pq.push({-dist[next.second], next.second});
+			}
+		}
+	}
+}
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int N, M, start, end;
+	cin >> N >> M;
+
+	for (int i = 0; i < M; i++)
+	{
+		int s, e, cost;
+		cin >> s >> e >> cost;
+		adj[s].push_back(make_pair(cost, e));
+	}
+	cin >> start >> end;
+	BFS(start);
+	cout << dist[end];
+	return 0;
+}
