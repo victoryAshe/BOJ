73d7b855bc8df7b95b2b9db015cda812f9eff0b6
Yeon <includeashe@gmail.com>
2024-11-14 07:53:24 +0900

    [Gold IV] Title: 사이클 게임, Time: 196 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 500001;
+int parent[MAX];
+int depth[MAX];
+
+int find(int i)
+{
+	if (i == parent[i]) return i;
+	return parent[i] = find(parent[i]);
+}
+
+void merge(int x, int y)
+{
+	x = find(x);
+	y = find(y);
+
+	if (x == y) return;
+	if (depth[x] > depth[y]) swap(x, y);
+	parent[x] = y;
+	if (depth[x] == depth[y]) depth[y]++;
+}
+
+bool isCycle(int first, int second)
+{
+	first = find(first);
+	second = find(second);
+	if (first == second) return true;
+	merge(first, second);
+	return false;
+}
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	int result = 0;
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		parent[i] = i;
+		depth[i] = 1;
+	}
+
+	for (int i = 1; i <= m; i++)
+	{
+		int first, second;
+		cin >> first >> second;
+		if (result == 0 && isCycle(first, second))
+		{
+			result = i;
+		}
+	}
+
+	cout << result;
+
+	return 0;
+}
