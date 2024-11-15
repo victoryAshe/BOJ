613a1d308b2573e338766871b92120d37563d04b
Yeon <includeashe@gmail.com>
2024-11-15 15:15:21 +0900

    [Gold III] Title: 중량제한, Time: 56 ms, Memory: 5556 KB -BaekjoonHub

---
+++ 백준/Gold/1939. 중량제한/중량제한.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+const int MAX = 10001;
+int maxCost = 0;
+vector<pair<int,int>> adj[MAX];
+bool visit[MAX];
+int Start, End;
+
+bool BFS(int curWeight)
+{
+	queue<int> Q;
+	Q.push(Start);
+	visit[Start] = true;
+
+	while (!Q.empty())
+	{
+		int curFactory = Q.front();
+		Q.pop();
+
+		if (curFactory == End) return true; // 도달 O
+
+		int size = adj[curFactory].size();
+		for (int i = 0; i < size; i++)
+		{
+			int nextFactory = adj[curFactory][i].first;
+			int nextWeight = adj[curFactory][i].second;
+			if (!visit[nextFactory] && curWeight <= nextWeight)
+			{
+				visit[nextFactory] = true;
+				Q.push(nextFactory);
+			}
+		}
+	}
+	return false; // 해당 weight를 기준으로 도달X
+}
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++)
+	{
+		int A, B, C;
+		cin >> A >> B >> C;
+		maxCost = max(maxCost, C);
+		adj[A].push_back({ B, C });
+		adj[B].push_back({ A, C });
+	}
+	cin >> Start >> End;
+
+	int low = 0, high = maxCost;
+	while (low <= high)
+	{
+		memset(visit, false, sizeof(visit));
+		int mid = (high + low) / 2;
+		if (BFS(mid)) low = mid + 1;
+		else high = mid - 1;
+	}
+
+	cout << high;
+
+	return 0;
+}
