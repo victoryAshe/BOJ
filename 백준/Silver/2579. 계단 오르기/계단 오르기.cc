a1d758c33984fa564a259d936935a9e0a38cfe2b
Yeon <includeashe@gmail.com>
2025-01-06 21:37:12 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -3,7 +3,6 @@
 using namespace std;
 
 const int MAX = 301;
-int score[MAX];
 int cache[MAX][3];
 
 int N;
@@ -11,13 +10,13 @@ int N;
 int main()
 {
     cin >> N;
-    for (int i = 1; i <= N; i++) cin >> score[i];
-    
-    for (int i = 1; i <= N + 1; i++)
+    for (int i = 1; i <= N; i++)
     {
+        int score;
+        cin >> score;
         cache[i][0] = max(cache[i - 1][1], cache[i - 1][2]);
-        cache[i][1] = cache[i - 1][0] + score[i];
-        cache[i][2] = cache[i - 1][1] + score[i];
+        cache[i][1] = cache[i - 1][0] + score;
+        cache[i][2] = cache[i - 1][1] + score;
     }
 
     cout << max(cache[N][1], cache[N][2]);
