336fc361cdc38d8f42e54505efc2835e32315e71
Yeon <includeashe@gmail.com>
2025-02-03 09:58:57 +0900

    [Silver III] Title: 어린 왕자, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.cc
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.cc
@@ -6,20 +6,18 @@ void Solution()
 {
 	pair<int, int> start, end;
 	int n;
-	int startCnt = 0, endCnt = 0;
+	int result = 0;
 
 	cin >> start.first >> start.second >> end.first >> end.second >> n;
 	for (int i = 0; i < n; i++)
 	{
 		pair<int, int> c; int r;
 		cin >> c.first >> c.second >> r;
-		int start_c = sqrt(pow(c.first - start.first, 2)+pow(c.second - start.second, 2));
-		int end_c = sqrt(pow(c.first - end.first, 2) + pow(c.second - end.second, 2));
-		if (start_c < r && end_c < r) continue;
-		if (start_c < r) startCnt++;
-		if (end_c < r) endCnt++;
+		bool isStartInCircle = sqrt(pow(c.first - start.first, 2)+pow(c.second - start.second, 2))<r;
+		bool isEndInCircle = sqrt(pow(c.first - end.first, 2) + pow(c.second - end.second, 2)) <r;
+        if(isStartInCircle!=isEndInCircle) result++;
 	}
-	cout << startCnt + endCnt << "\n";
+	cout << result << "\n";
 
 }
 
