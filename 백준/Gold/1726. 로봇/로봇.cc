9650a0c62d731b883baa12e51adae2a633e945eb
Yeon <includeashe@gmail.com>
2025-03-06 14:45:53 +0900

    [Gold III] Title: 로봇, Time: 0 ms, Memory: 2224 KB -BaekjoonHub

---
+++ 백준/Gold/1726. 로봇/로봇.cc
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+const int INF = 1e9;
+const int NINF = -1e9;
+
+int M, N;
+int times[101][101][5];
+
+int Dir[5][2] = { {0, 0}, {0,1}, {0,-1}, {1,0},{-1,0} };
+int getTwice[5] = { NINF, 2, 1, 4, 3 };
+
+struct State {
+	int y, x, dir;
+};
+
+queue<State> Q;
+
+bool isValid(int ny, int nx)
+{
+	return !(ny<1 || ny > M || nx<1 || nx>N);
+}
+
+int CalcTime(State cur, int nextDir)
+{
+	int returnTime = 0;
+	if (nextDir != cur.dir)
+		returnTime = (nextDir == getTwice[cur.dir] ? 2 : 1);
+	return returnTime;
+}
+
+void BFS(State& start, State& end)
+{
+	Q.push(start);
+	times[start.y][start.x][start.dir] = 0;
+	
+	while (!Q.empty())
+	{
+		State cur = Q.front();
+		Q.pop();
+
+		for (int i = 1; i <= 4; i++)
+		{
+			int ny = cur.y;
+			int nx = cur.x;
+			int nextTime = times[cur.y][cur.x][cur.dir] + (i!=0);
+			
+			for (int j = 1; j <= 4; j++)
+			{
+				if (i == j) continue;
+				int turnedTime = nextTime + CalcTime(cur, j);
+			}
+
+			nextTime += CalcTime(cur, i);
+
+			if (i == 0) continue;
+			for (int j = 1; j <= 3; j++)
+			{
+				ny += Dir[i][0]; 
+				nx += Dir[i][1];
+				
+				if (!isValid(ny, nx)) break;
+				if (times[ny][nx][0] == NINF) break;
+				if (times[ny][nx][i] > nextTime)
+				{
+					times[ny][nx][i] = nextTime;
+					Q.push({ ny,nx,i });
+				}
+			}
+		}
+	}
+
+	int ans = INF;
+	for (int i = 1; i <= 4; i++)
+	{
+		if (times[end.y][end.x][i] != INF)
+			ans = min(ans, times[end.y][end.x][i] + CalcTime({ end.y,end.x,i }, end.dir));
+	}
+
+	cout << ans ;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> M >> N;
+	for (int i = 1; i <= M; i++)
+	{
+		bool isNotRoute;
+		for (int j = 1; j <= N; j++)
+		{
+			cin >> isNotRoute;
+			if (isNotRoute) times[i][j][0] = NINF;
+			else
+			{
+				for (int k = 0; k <= 4; k++) 
+					times[i][j][k] = INF;
+			}
+			
+		}
+	}
+
+	int y, x, dir;
+	cin >> y >> x >> dir;
+	State Start = { y,x,dir};
+	cin >> y >> x >> dir;
+	State End = { y,x,dir };
+
+	BFS(Start, End);
+
+	return 0;
+}
