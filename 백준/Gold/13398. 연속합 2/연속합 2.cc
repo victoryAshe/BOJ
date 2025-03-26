2079b9165c6192eca92442bdd27153ba3141feaf
Yeon <includeashe@gmail.com>
2025-03-27 05:13:08 +0900

    [Gold V] Title: 연속합 2, Time: 8 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.cc
+++ 백준/Gold/13398. 연속합 2/연속합 2.cc
@@ -4,15 +4,15 @@ using namespace std;
 const int MAX = 100001;
 int num[MAX];
 long long dp[2][MAX];
-long long biggest = -1e9 -1;
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+    cin.tie(NULL);
 
     int n; cin >> n;
     for (int i = 0; i < n; i++) cin >> num[i];
-    dp[0][0] = dp[1][0] = biggest = num[0];
+    long long biggest = dp[0][0] = dp[1][0] = num[0];
+
     for (int i = 1; i < n; i++)
     {
         long long prev = dp[0][i - 1], prevDel = dp[1][i - 1];
