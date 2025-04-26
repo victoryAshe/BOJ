459cde6eaca70e3dca331c99225fd90412975e4d
Yeon <includeashe@gmail.com>
2025-04-26 21:20:08 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cout.tie(NULL);
+    int N; cin >> N;
+    for(int i = 0; i<N; i++) cout << "LoveisKoreaUniversity ";
+    return 0;
+}
