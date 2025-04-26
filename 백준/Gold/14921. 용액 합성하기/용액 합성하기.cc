65aaa5f8857136352504d7be62ae7bfba7dd1de3
Yeon <includeashe@gmail.com>
2025-04-26 21:23:31 +0900

    [Gold V] Title: 용액 합성하기, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/14921. 용액 합성하기/용액 합성하기.cc
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cmath>
-#include <algorithm>
 using namespace std;
 
 int pro[100'001];
@@ -10,39 +9,26 @@ void MakeResult(int left, int right)
 {
     while (left < right)
     {
-        auto target = lower_bound(pro + left + 1, pro + right, -pro[left]);
-        int curResult1 = pro[left] + *target;
-        int curResult2 = pro[left] + *(target - 1);
-        int curResult, idx;
-        if (abs(curResult1) < abs(curResult2) || left == target - pro - 1)
-        {
-            curResult = curResult1;
-            idx = target - pro;
-        }
-        else
-        {
-            curResult = curResult2;
-            idx = target - pro - 1;
-        }
-
+        int curResult = pro[left] + pro[right];
         if (abs(curResult) < abs(result))
         {
             result = curResult;
-            right = idx;
         }
-        left++;
+        if (curResult < 0) left++;
+        else if (curResult > 0) right--;
+        else break;
     }
 }
 
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(NULL); 
+    cin.tie(NULL);
 
     int N;
     cin >> N;
     for (int i = 0; i < N; i++) cin >> pro[i];
-    MakeResult(0, N-1);
+    MakeResult(0, N - 1);
     cout << result;
     return 0;
 }
