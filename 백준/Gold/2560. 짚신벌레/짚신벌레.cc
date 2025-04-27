34e991e7ce6528c6727207c24f2d32b0cf7e741b
Yeon <includeashe@gmail.com>
2025-04-27 15:52:50 +0900

    [Gold III] Title: 짚신벌레, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/2560. 짚신벌레/짚신벌레.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+const int MOD = 1000;
+int a, b, d, N;
+int dp[1'000'001];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> a >> b >> d >> N;
+
+	for (int day = 0; day < a; day++) dp[day] = 1;
+	for (int day = a; day <= N; day++)
+	{
+		dp[day] = (dp[day - 1] + dp[day - a]) % MOD;
+		if (day >= b) dp[day] = (dp[day] - dp[day - b] + 1000) % MOD;
+	}
+
+	int answer = dp[N];
+	if (N >= d) answer = (answer + 1000 - dp[N - d]) % MOD;
+
+	cout << answer;
+	return 0;
+}
