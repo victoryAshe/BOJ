57099c071ce87bf9fab232cbd85b028671366114
Yeon <includeashe@gmail.com>
2025-06-09 06:18:54 +0900

    [Silver I] Title: 카잉 달력, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int GCD(int M, int N)
+{
+	return M % N ? GCD(N, M % N) : N;
+}
+
+int LCM(int M, int N)
+{
+	return M * N / GCD(M, N);
+}
+
+void Solution()
+{
+	int M, N, x, y;
+	cin >> M >> N >> x >> y;
+	int maxYear = LCM(M, N);
+	
+	bool isValid = true;
+	while (true)
+	{
+		if (x > y) y += N;
+		else if (x < y) x += M;
+		else break;
+
+		if (x > maxYear || y > maxYear)
+		{
+			isValid = false;
+			break;
+		}
+	}
+
+	if (isValid) cout << x;
+	else cout << "-1";
+	cout << '\n';
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int T; cin >> T;
+	for (int t = 0; t < T; t++)
+	{
+		Solution();
+	}
+
+	return 0;
+}
