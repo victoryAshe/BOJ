7abd94dd3043dc2759b749cee6f5de5faacc040c
Yeon <includeashe@gmail.com>
2025-06-11 07:12:38 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> Point;
+typedef pair<Point, Point> Segment;
+
+ll ccw(Point p1, Point p2, Point p3)
+{
+	ll ret = (p1.first * p2.second + p2.first * p3.second + p3.first * p1.second)
+		- (p2.first * p1.second + p3.first * p2.second + p1.first * p3.second);
+	if (ret < 0) return -1;
+	if (ret > 0) return 1;
+	return 0;
+}
+
+bool isIntersect(Segment s1, Segment s2)
+{
+	Point p1 = s1.first, p2 = s1.second;
+	Point p3 = s2.first, p4 = s2.second;
+
+	ll p1p2 = ccw(p1, p2, p3) * ccw(p1, p2, p4);
+	ll p3p4 = ccw(p3, p4, p1) * ccw(p3, p4, p2);
+
+	if (p1p2 == 0 && p3p4 == 0)
+	{
+		if (p1 > p2) swap(p1, p2);
+		if (p3 > p4) swap(p3, p4);
+
+		return (p2 >= p3 && p4 >= p1);
+	}
+	return (p1p2 <= 0 && p3p4 <= 0);
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	Segment s1, s2;
+	cin >> s1.first.first >> s1.first.second >> s1.second.first >> s1.second.second;
+	cin >> s2.first.first >> s2.first.second >> s2.second.first >> s2.second.second;
+
+	cout << isIntersect(s1, s2);
+
+	return 0;
+}
