e96d3404fcbd0e841dabc01978f3febd6fd17d88
Yeon <includeashe@gmail.com>
2025-12-04 01:48:00 +0900

    [Bronze V] Title: 가희와 교통 요금, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/34665. 가희와 교통 요금/가희와 교통 요금.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    string A, B;
+    cin >> A >> B;
+
+    cout << (A == B ? 0 : 1550);
+
+    return 0;
+}
