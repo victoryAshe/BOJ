f60cfd647e53e6f157f42fcc21d6b2f00c2649d3
Yeon <includeashe@gmail.com>
2026-04-16 19:24:34 +0900

    [Platinum V] Title: 행성 터널, Time: 116 ms, Memory: 14492 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.cc
+++ 백준/Platinum/2887. 행성 터널/행성 터널.cc
@@ -2,49 +2,49 @@
 #include <cmath>
 #include <algorithm>
 #include <queue>
-using namespace std;
 
-const int MAX = 100001;
+const int MAX = 100'001;
+using P = std::pair<int, int>;
+
 int parent[MAX];
 int depth[MAX];
 
-pair<int, int> X[MAX];
-pair<int, int> Y[MAX];
-pair<int, int> Z[MAX]; 
-priority_queue<pair<int, pair<int, int>>> edges;
+P X[MAX];
+P Y[MAX];
+P Z[MAX];
+std::priority_queue<std::pair<int, P>> edges;
 
 long long answer = 0;
 
-int find(int i)
+int Find(int i)
 {
 	if (parent[i] == i) return i;
-	return parent[i] = find(parent[i]);
+	return parent[i] = Find(parent[i]);
 }
 
-void merge(int u, int v)
+void Merge(int u, int v)
 {
-	u = find(u);
-	v = find(v);
+	u = Find(u);
+	v = Find(v);
 	if (u == v) return;
 
-	if (depth[u] > depth[v]) swap(u, v);
+	if (depth[u] > depth[v]) std::swap(u, v);
 	parent[u] = v;
 	if (depth[u] == depth[v]) depth[v]++;
 }
 
 int main()
 {
-	cin.tie(NULL);
-	ios::sync_with_stdio(false);
+	std::ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
 
-	int N; 
-	cin >> N;
+	int N;
+	std::cin >> N;
 
 	for (int i = 1; i <= N; i++)
 	{
 		int x, y, z;
-		cin >> x >> y >> z;
-		
+		std::cin >> x >> y >> z;
+
 		X[i] = { x, i };
 		Y[i] = { y, i };
 		Z[i] = { z, i };
@@ -53,9 +53,9 @@ int main()
 		depth[i] = 1;
 	}
 
-	sort(X+1, X + N + 1);
-	sort(Y+1, Y + N + 1);
-	sort(Z+1, Z + N + 1);
+	sort(X + 1, X + N + 1);
+	sort(Y + 1, Y + N + 1);
+	sort(Z + 1, Z + N + 1);
 
 	for (int i = 1; i < N; i++)
 	{
@@ -67,16 +67,16 @@ int main()
 	while (!edges.empty())
 	{
 		int dist = -edges.top().first;
-		int a = edges.top().second.first;
-		int b = edges.top().second.second;
+		int u = edges.top().second.first;
+		int v = edges.top().second.second;
 		edges.pop();
-		if (find(a) == find(b)) continue;
+		if (Find(u) == Find(v)) continue;
 
-		merge(a, b);
+		Merge(u, v);
 		answer += dist;
 	}
 
-	cout << answer;
+	std::cout << answer;
 
 	return 0;
 }
